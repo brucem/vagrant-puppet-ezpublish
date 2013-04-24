086d9b74782b3aee4e5781cc20b5ee0f4c30dd89
Bruce Morrison <bruce.morrison@stuffandcontent.com>
2013-04-24 12:11:20 +1000

    Install puppet modules on the VM as part of the bootstrap process. This negates the need to have puppet & ruby installed on the host.

--- bootstrap-vagrant-ubuntu.sh
+++ bootstrap-vagrant-ubuntu.sh
@@ -3,19 +3,26 @@
 if [ ! -e ~/.bootstrap-vagrant ]; then
     export DEBIAN_FRONTEND=noninteractive
 
+    # Add official Puppet repo
+    wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+    dpkg -i puppetlabs-release-precise.deb
+    rm puppetlabs-release-precise.deb
+
     # Update sources
     apt-get update
     apt-get -y upgrade
 
-    apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-    apt-get -y install vim
-
     apt-get -y install puppet
-
-    apt-get -y install augeas-lenses augeas-tools libaugeas-ruby
 
     apt-get -y autoremove
     apt-get clean
+
+    # Install required puppet modules
+    PUPPET_MODULES=('brucem/ezpublish' 'puppetlabs/ntp')
+    for ((i = 0; i < ${#PUPPET_MODULES[@]}; i++))
+    do
+        puppet module install ${PUPPET_MODULES[$i]}
+    done
 
     touch ~/.bootstrap-vagrant
 fi
