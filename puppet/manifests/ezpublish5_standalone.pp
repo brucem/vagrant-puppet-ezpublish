ec00d7c8a1bdf943879c52f01a6bb0818c765de7
Bruce Morrison <bruce@stuffandcontent.com>
2013-12-08 05:20:33 +0000

    Update to use eZ Publish Community Project 2013.11

--- puppet/manifests/ezpublish5_standalone.pp
+++ puppet/manifests/ezpublish5_standalone.pp
@@ -39,10 +39,10 @@ node default inherits base {
     db_pass => 'password',
   }
 
-  # Install eZ Publish 5 (Community Project 2013.09) from url in the default
+  # Install eZ Publish 5 (Community Project 2013.11) from url in the default
   # DocRoot
-  ezpublish::install { 'eZ Publish Community Project 2013.09':
-    src     => 'http://share.ez.no/content/download/153730/907395/version/1/file/ezpublish5_community_project-2013.09-gpl-full.tar.gz',
+  ezpublish::install { 'eZ Publish Community Project 2013.11':
+    src     => 'http://share.ez.no/content/download/154571/912584/version/1/file/ezpublish5_community_project-2013.11-gpl-full.tar.gz',
     dest    => '/var/www/default',
     require => Class['ezpublish::default']
   }
